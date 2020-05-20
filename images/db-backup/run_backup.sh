ebec94bc24d454aa79b63269fc8eeb84212a6615
Christoph <email@ckloth.de>
2020-05-20 15:15:23 +0200

    fix backup only nextcloud db

--- images/db-backup/run_backup.sh
+++ images/db-backup/run_backup.sh
@@ -3,6 +3,6 @@
 echo -n "executed run_backup at $(date)"
 date=`date +%y%m%d-%H%M`
 
-mysqldump -A -h $DB_BACKUP_HOST --password=$DB_BACKUP_PASSWORD -B $DB_BACKUP_DATABASE > /backup/$DB_BACKUP_NAME\_$date.sql && echo " - successful"
-mysqldump -A -h $DB_BACKUP_HOST --password=$DB_BACKUP_PASSWORD -B $DB_BACKUP_DATABASE > /backup-latest/$DB_BACKUP_NAME\_latest.sql && echo " - successful"
+mysqldump -A -h $DB_BACKUP_HOST --password=$DB_BACKUP_PASSWORD --database=$DB_BACKUP_DATABASE > /backup/$DB_BACKUP_NAME\_$date.sql && echo " - successful"
+mysqldump -A -h $DB_BACKUP_HOST --password=$DB_BACKUP_PASSWORD --database=$DB_BACKUP_DATABASE > /backup-latest/$DB_BACKUP_NAME\_latest.sql && echo " - successful"
 
